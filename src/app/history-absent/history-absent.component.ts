11c410eb28ca6d5e713ae42cc859e9553779bfe1
jayeshgajare1390 <jayeshgajare2003@gmail.com>
2023-10-08 12:00:36 +0530

    ok

--- src/app/history-absent/history-absent.component.ts
+++ src/app/history-absent/history-absent.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { trigger, transition, style, animate } from '@angular/animations';
-import { HttpHeaders } from '@angular/common/http';
 
 interface AbsentStudent {
   prn: number;
@@ -12,8 +11,6 @@ interface AbsentStudent {
   coursecode: string;
   block_no: number;
   date: string;
-  attendance: boolean;
-  malpractice: boolean;
 }
 
 @Component({
@@ -41,14 +38,13 @@ export class HistoryAbsentComponent implements OnInit {
   selectedProgram = '';
   selectedDate = '';
   isLoading: boolean = true;
-  displayedColumns: string[] = ['prn', 'studentName', 'program', 'class', 'course', 'courseCode', 'blockNo', 'date'];
+  displayedColumns: string[] = ['prn', 'studentname', 'year', 'programname', 'course', 'coursecode', 'block_no', 'date'];
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.fetchAbsentStudents();
   }
-  
 
   fetchAbsentStudents(): void {
     this.http.get<AbsentStudent[]>('http://localhost:8000/api/absent-students/all').subscribe(
@@ -57,58 +53,78 @@ export class HistoryAbsentComponent implements OnInit {
         this.absentStudents = [...data]; // Make a copy
         console.log(this.originalAbsentStudents);
         this.isLoading = false; // Set isLoading to false once data is received
-
       },
       (error) => {
         console.error('Error fetching absent students:', error);
         this.isLoading = false; // Make sure to handle errors and set isLoading to false
-
       }
     );
   }
+
+  // deleteRecord(prn: number, blockNumber: number, date: string): void {
+  //   const url = `http://localhost:8000/api/absent-students/delete`;
+
+  //   // Send the prn parameter as a query parameter in the URL
+  //   const params = {
+  //     prn: prn.toString() // Convert prn to string if necessary
+  //   };
+
+  //   this.http.post(url, params).subscribe(
+  //     (response) => {
+  //       console.log('Record deleted successfully', response);
+  //       // Update your local data if needed
+  //     },
+  //     (error) => {
+  //       console.error('Error deleting record:', error);
+  //     }
+  //   );
+  // }
+
   deleteRecord(prn: number, blockNumber: number, date: string) {
     const url = `http://localhost:8000/api/absent-students/delete`;
   
     const params = {
-      prn: prn,
-      blockNumber: blockNumber,
-      date: date
+        prn: prn,
+        blockNumber: blockNumber,
+        date: date
     };
-  
+
     this.http.post(url, params).subscribe(
-      (response) => {
-        console.log('Record deleted successfully', response);
-        // Update your local data if needed
-      },
-      (error) => {
-        console.error('Error deleting record:', error);
-      }
+        (response: any) => {
+            console.log('Record deleted successfully', response.message);
+            // Update your local data if needed
+        },
+        (error) => {
+            console.error('Error deleting record:', error);
+        }
     );
-  }
+}
+
   
   
+
   applyFilters(): void {
     let filteredStudents = this.originalAbsentStudents;
-  
+
     if (this.selectedClass) {
       filteredStudents = filteredStudents.filter(student => student.year.includes(this.selectedClass));
     }
-  
+
     if (this.selectedProgram) {
       filteredStudents = filteredStudents.filter(student => student.programname.includes(this.selectedProgram));
     }
-  
+
     if (this.selectedDate) {
       filteredStudents = filteredStudents.filter(student => student.date === this.selectedDate);
     }
-  
+
     this.absentStudents = filteredStudents;
   }
-  clearFilters(): void {
-  this.selectedClass = '';
-  this.selectedProgram = '';
-  this.selectedDate = '';
-  this.absentStudents = this.originalAbsentStudents;
-}
 
+  clearFilters(): void {
+    this.selectedClass = '';
+    this.selectedProgram = '';
+    this.selectedDate = '';
+    this.absentStudents = this.originalAbsentStudents;
+  }
 }
